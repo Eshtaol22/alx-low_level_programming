9a2c6d604d02355f01a599b8fe507aea5341e1c5
Eshtaol22 <feben.alebe@gmail.com>
2023-10-18 10:33:12 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - this program write a line by using the puts function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
